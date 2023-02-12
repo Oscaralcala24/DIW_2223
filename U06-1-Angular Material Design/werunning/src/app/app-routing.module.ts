aed7563da640445a80c50eb4b59ea3f3cd71d535
Oscaralcala24 <alum.oalcalae.2020@iesalixar.org>
2023-02-12 16:45:36 +0100

    accesibilidad

--- U06-1-Angular Material Design/werunning/src/app/app-routing.module.ts
+++ U06-1-Angular Material Design/werunning/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './modules/registration/registration.component';
+import { HomeComponent } from './modules/home/home.component';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+const routes: Routes = [{
+  path: '',
+  component: SkeletonComponent,
+  children: [
+    { path: '', component: HomeComponent },
+    { path: 'register', component: RegistrationComponent }
+  ]
 
-const routes: Routes = [];
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
